1af6fcefc7f1ae8e0010aa63ffb41545a019720d
AlimEmon0333 <alimemon0333022@gmail.com>
2024-05-21 21:57:17 +0500

    Updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './components/navbar/App';
 import "./App.css";
+import Hero from './components/heroSection/App';
 
 function App() {
   return (
     <div className="App">
     <Navbar />
+    <Hero />
     </div>
   );
 }
